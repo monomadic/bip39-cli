ee85cd6610fefaa6ba9b7b72ce2161c810cf6a48
monomadic <monomadic.crypto>
2022-08-16 00:28:07 +0700

    update deps

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,10 @@
-use bip39::{Mnemonic, MnemonicType, Language, Seed};
+use bip39::{Mnemonic, Language};
 
 fn main() {
     println!("BIP39 Mnemonic");
     println!("==============\n");
 
-    // create a new randomly generated mnemonic phrase
-    let mnemonic = Mnemonic::new(MnemonicType::Words12, Language::English);
-    
-    // get the phrase
-    let phrase: &str = mnemonic.phrase();
-    println!("Mnemonic Phrase:\n{}\n", phrase);
-    
-    // get the HD wallet seed
-    let seed = Seed::new(&mnemonic, "");
-    
-    // get the HD wallet seed as raw bytes
-    let seed_bytes: &[u8] = seed.as_bytes();
-    
-    // print the HD wallet seed as a hex string
-    println!("Seed (Private Key):\n{:?}", seed);
+		let mnemonic = Mnemonic::generate_in(Language::English, 12).unwrap();
+
+    println!("Mnemonic Phrase:\n{}\n", mnemonic);
 }
